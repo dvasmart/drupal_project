b8986b83739283b08eba832a54b0337382395328
Vitalii Dronskyi <dvasmart@gmail.com>
2022-05-11 15:14:27 +0300

    module custom page

---
+++ web/modules/custom/welcome_module/src/Controller/WelcomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\welcome_module\Controller;
+
+class WelcomeController 
+{
+    public function welcome() 
+    {
+        return [
+            '#markup' => 'Welcome to our Website.'
+        ];
+    }
+}
