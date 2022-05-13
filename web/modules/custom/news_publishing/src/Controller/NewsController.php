8aa82a3915042c6376127a54ec683cdb57fe8fba
Vitalii Dronskyi <dvasmart@gmail.com>
2022-05-14 01:17:02 +0300

    news_publishing - show news by id

--- web/modules/custom/news_publishing/src/Controller/NewsController.php
+++ web/modules/custom/news_publishing/src/Controller/NewsController.php
@@ -8,27 +8,46 @@ use Drupal\Core\Controller\ControllerBase;
  * Provides route responses for module.
  */
 class NewsController extends ControllerBase 
-{ 
+{
+  private $entity_type = 'node';
 
   /**
-   * Returns page with latest news.
+   * Returns page with latest news
    * @return array
    */
   public function newsLatest() 
+  {  
+    $storage = \Drupal::entityTypeManager()->getStorage($this->entity_type);
+    $lastId = $storage->getQuery()->condition('status', 1)->condition('type', 'news')->sort('nid', 'DESC')->pager(1)->execute();    
+    $storage = \Drupal::entityTypeManager()->getStorage($this->entity_type);
+    $node = $storage->load(reset($lastId));
+    $viewBuilder = \Drupal::entityTypeManager()->getViewBuilder($this->entity_type);
+    $list = $viewBuilder->view($node, 'teaser');
+    $result = render($list);
+
+    return ['#markup' => $result];
+  }
+
+  /**
+   * Returns page with news by id
+   * @return array
+   */
+  public function newsById($id) 
   {
-    $entity_type = 'node';
+    $news = [];
 
-    $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
-    $lastIds = $storage->getQuery()->condition('status', 1)->condition('type', 'news')->sort('nid', 'DESC')->pager(1)->execute();
+    $nodeStorage = \Drupal::entityTypeManager()->getStorage($this->entity_type);
+    $id = $nodeStorage->getQuery()->condition('status', '1')->condition('type', 'news')->condition('field_term_reference', $id)->execute();
+    $viewBuilder = \Drupal::entityTypeManager()->getViewBuilder($this->entity_type);
 
-    $builder = \Drupal::entityTypeManager()->getViewBuilder($entity_type);
-    $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
-    $node = $storage->load(reset($lastIds));
-    $build = $builder->view($node, 'teaser');
-    $output = render($build);
+    $itemsStorage = \Drupal::entityTypeManager()->getStorage($this->entity_type);  
+    foreach ($itemsStorage->loadMultiple($id) as $ids) {
+      $news[] = $ids;
+    }
 
-    return ['#markup' => $output];
+    $list = $viewBuilder->viewMultiple($news, 'teaser');
 
-  } 
+    return $list;
+  }
 
 }
